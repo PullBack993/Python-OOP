e502cf4b14338bc6bdc819eddf13d942950340f1
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2021-07-16 16:22:41 +0200

    add

---
+++ 1.Defining Classes - Exercise/8.pokemon_battle/project/trainer.py
@@ -0,0 +1,25 @@
+class Trainer:
+    def __init__(self, name: str):
+        self.name = name
+        self.pokemon = []
+
+    def add_pokemon(self, pokemon):
+        if pokemon in self.pokemon:
+            return "This pokemon is already caught"
+        self.pokemon.append(pokemon)
+        return f"Caught {pokemon.pokemon_details()}"
+
+    def release_pokemon(self, pokemon_name: str):
+        try:
+            filtered_pokemon = [p for p in self.pokemon if p.name == pokemon_name][0]
+            self.pokemon.remove(filtered_pokemon)
+            return f"You have released {pokemon_name}"
+        except IndexError:
+            return "Pokemon is not caught"
+
+    def trainer_data(self):
+        data = f"Pokemon Trainer {self.name}\n" \
+               f"Pokemon count {len(self.pokemon)}\n"
+        for animal in self.pokemon:
+            data += f"- {animal.pokemon_details()}\n"
+        return data
