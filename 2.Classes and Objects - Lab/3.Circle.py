7baf5255d81ea23a895d25e9b9ebad67be35f515
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2021-07-20 13:30:35 +0200

    add

---
+++ 2.Classes and Objects - Lab/3.Circle.py
@@ -0,0 +1,22 @@
+class Circle:
+    pi = 3.14
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+    def get_area(self):
+        result = Circle.pi * (self.radius ** 2)
+        return result
+
+    def get_circumference(self):
+        result = 2 * Circle.pi * self.radius
+        return result
+
+
+circle = Circle(10)
+circle.set_radius(12)
+print(circle.get_area())
+print(circle.get_circumference())
