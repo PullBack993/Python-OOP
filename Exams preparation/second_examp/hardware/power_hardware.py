ecbde759fa11f63166b610b908ac4ec5377d3346
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2021-11-14 11:58:56 +0100

    add

---
+++ Exams preparation/second_examp/hardware/power_hardware.py
@@ -0,0 +1,8 @@
+from project.hardware.hardware import Hardware
+
+
+class PowerHardware(Hardware):
+    def __init__(self, name, capacity, memory):
+        super().__init__(name, "Power", (capacity*0.25), int(memory * 1.75))
+
+
